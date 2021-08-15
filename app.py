5fdb593404573e437c629b931604244169c6712f
Carly Pecora <carly@carlypecora.com>
2021-08-14 21:03:43 -0400

    add mongodb and update routes

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 # ~flask-api/app.py
 
-from flask import Flask, jsonify, request
-
+from flask import Flask, request, Response
+from database.db import initialize_db
+from database.models import Book
 app = Flask(__name__)
 
-books = []
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/book-api'
+}
 
-@app.route('/')
+initialize_db(app)
+
+@app.route('/books')
 def get_books():
-    return jsonify(books)
+    books = Book.objects().to_json()
+    return Response(books, mimetype="application/json", status=200)
+
+@app.route('/books/<id>')
+def get_book(id):
+    book = Book.objects.get(id=id).to_json()
+    return Response(book, mimetype="application/json", status=200)
 
 @app.route('/books', methods=['POST'])
 def add_book():
-    book = request.get_json()
-    books.append(book)
-    return {'id': books.index(book)}, 200
+    body = request.get_json()
+    book = Book(**body).save()
+    id = book.id
+    return {'id': str(id)}, 201
 
-@app.route('/books/<int:index>', methods=['PUT'])
-def update_book(index):
-    book = request.get_json()
-    books[index] = book
-    return jsonify(books[index]), 200
+@app.route('/books/<id>', methods=['PUT'])
+def update_book(id):
+    body = request.get_json()
+    Book.objects.get(id=id).update(**body)
+    return '', 200
 
-@app.route('/books/<int:index>', methods=['DELETE'])
-def delete_book(index):
-    books.pop(index)
-    return 'None', 200
+@app.route('/books/<id>', methods=['DELETE'])
+def delete_book(id):
+    Book.objects.get(id=id).delete()
+    return '', 200
 
 
 app.run()
